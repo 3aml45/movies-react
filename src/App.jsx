1f4ad4adb4a146188bbae1261efda6a89cfdfdde
unknown <eamlbtc@gmail.com>
2021-12-20 21:38:42 -0400

    react-router-dom done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@
 import styles from './css/App.module.css';
 // import components
 import { MoviesGrid } from './MoviesGrid';
+// import from react-router-dom
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export function App() {
   return (
-    <div className={styles.app}>
+    <Router className={styles.app}>
       <header>
         <h1 className={styles.title}>Movies</h1>
       </header>
       <main>
-        <MoviesGrid />
+        <Switch>
+          <Route path="/movie">Movie</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+        {/* <MoviesGrid /> */}
       </main>
-    </div>
+    </Router>
   );
 }
 
