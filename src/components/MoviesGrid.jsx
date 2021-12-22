fa7e5ddebca4c438569b81087d7d62eaffe916d4
unknown <eamlbtc@gmail.com>
2021-12-21 20:59:48 -0400

    done video 2

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -6,24 +6,24 @@ import styles from '../css/MoviesGrid.module.css';
 import { MovieCard } from './MovieCard';
 // import from react
 import { useEffect, useState } from 'react';
+// import custom functions
+import { get } from '../utils/httpClient';
+
 
 
 export function MoviesGrid() {
 	const [movies, setMovies] = useState([]);
+	
 	useEffect(()=>{
-		fetch(
-			"https://api.themoviedb.org/3/discover/movie/?api_key=9e55e70322adb52b8edc5b55564a0a2d"
-		).then(result => result.json()).then(data => {
-			setMovies(data.results);
-			console.log(movies)
-		})
-		// fetch("https://api.themoviedb.org/3/discover/movie", {
-		// 	headers: {
-		// 		Authorization:
-		// 			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
-		// 		"Content-Type": "application/json;charset=utf-8",
-		// 	},
+		// fetch(
+		// 	"https://api.themoviedb.org/3/discover/movie/?api_key=9e55e70322adb52b8edc5b55564a0a2d"
+		// ).then(result => result.json()).then(data => {
+		// 	setMovies(data.results);
+		// 	console.log(movies)
 		// })
+		get('/discover/movie').then(data=>{
+			setMovies(data.results);
+		})
 	}, []);
 	return (
 		<ul className={styles.moviesGrid}>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,9 +1,26 @@
 // import styles
 import styles from '../css/MovieDetails.module.css';
 //import movie
-import movie from './movie.json';
+// import movie from './movie.json';
+// import from react-router-dom
+import { useParams } from 'react-router-dom';
+// import custom functions
+import { get } from '../utils/httpClient';
+// import form react
+import { useEffect, useState } from 'react';
 
 export function MovieDetails() {
+	const { movieId } = useParams();
+	const [movie, setMovie] = useState(null);
+	useEffect(()=>{
+		get('/movie/' + movieId).then(data=>{
+			setMovie(data)
+			console.log(movie)
+		})
+	},[movieId])
+	if(!movie){
+		return null;
+	}
 	const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 	return (
 		<div className={styles.detailsContainer}>
